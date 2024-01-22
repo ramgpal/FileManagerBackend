c9c7dcb7910d0743fe628ddda2040cb041323ed9
ramgpal <ramgpal912@gmail.com>
2024-01-23 01:22:05 +0530

    2ndCommit

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const {imageUpload, videoUpload, imageSizeReducer, localFileUpload} = require("../controllers/fileUpload");
 
 //api route 
-
 router.post("/localFileUpload", localFileUpload);
 router.post("/imageUpload", imageUpload);
 router.post("/videoUpload", videoUpload);
